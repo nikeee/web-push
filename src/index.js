82165c04301bc247c75a60896dcf08e1ee4fa1f7
Niklas Mollenhauer <nikeee@outlook.com>
2026-04-21 21:06:44 +0200

    Remove singleton instance

--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ const WebPushLib = require('./web-push-lib.js');
 const WebPushError = require('./web-push-error.js');
 const WebPushConstants = require('./web-push-constants.js');
 
-const webPush = new WebPushLib();
-
 export default {
-  WebPushError: WebPushError,
+  WebPushLib,
+  WebPushError,
   supportedContentEncodings: WebPushConstants.supportedContentEncodings,
   encrypt: encryptionHelper.encrypt,
   getVapidHeaders: vapidHelper.getVapidHeaders,
   generateVAPIDKeys: vapidHelper.generateVAPIDKeys,
-  setGCMAPIKey: webPush.setGCMAPIKey,
-  setVapidDetails: webPush.setVapidDetails,
-  generateRequestDetails: webPush.generateRequestDetails,
-  sendNotification: webPush.sendNotification.bind(webPush)
 };
