6d01df5a6e939fbaa4aadaef6057ffb5227642fc
Niklas Mollenhauer <nikeee@outlook.com>
2026-04-21 20:58:45 +0200

    Make WebPushError a class

--- src/web-push-error.js
+++ src/web-push-error.js
@@ -1,14 +1,10 @@
-function WebPushError(message, statusCode, headers, body, endpoint) {
-  Error.captureStackTrace(this, this.constructor);
-
-  this.name = this.constructor.name;
-  this.message = message;
-  this.statusCode = statusCode;
-  this.headers = headers;
-  this.body = body;
-  this.endpoint = endpoint;
-}
-
-require('util').inherits(WebPushError, Error);
-
-module.exports = WebPushError;
+module.exports = class WebPushError extends Error {
+  constructor(message, statusCode, headers, body, endpoint) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.headers = headers;
+    this.body = body;
+    this.endpoint = endpoint;
+  }
+};
