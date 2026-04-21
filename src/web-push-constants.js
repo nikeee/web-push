9f775fa613ee11d830a456943562d21b68f9d134
Niklas Mollenhauer <nikeee@outlook.com>
2026-04-21 20:58:55 +0200

    Inline constants

--- src/web-push-constants.js
+++ src/web-push-constants.js
@@ -1,15 +1,12 @@
-const WebPushConstants = {};
-
-WebPushConstants.supportedContentEncodings = {
-  AES_GCM: 'aesgcm',
-  AES_128_GCM: 'aes128gcm'
+module.exports = {
+  supportedContentEncodings: {
+    AES_GCM: 'aesgcm',
+    AES_128_GCM: 'aes128gcm'
+  },
+  supportedUrgency: {
+    VERY_LOW: 'very-low',
+    LOW: 'low',
+    NORMAL: 'normal',
+    HIGH: 'high'
+  },
 };
-
-WebPushConstants.supportedUrgency = {
-  VERY_LOW: 'very-low',
-  LOW: 'low',
-  NORMAL: 'normal',
-  HIGH: 'high'
-};
-
-module.exports = WebPushConstants;
